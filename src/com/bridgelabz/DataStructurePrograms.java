fc3afa321035a0997b0d85150627c6f410be2e7d
Arun <rarnkmrbridgelabz.com>
2022-04-06 23:55:53 +0530

    Adding Welcome Message

---
+++ src/com/bridgelabz/DataStructurePrograms.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class DataStructurePrograms {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Data Structure Programs");
+    }
+}
